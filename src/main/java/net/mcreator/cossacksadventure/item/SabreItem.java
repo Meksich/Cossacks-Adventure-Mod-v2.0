8bb2f5e03b0bc193e33741d7c270b5b92eacf34d
MykhailoPolnyi <mykhailo.polnyi.knm.2020@plnu.ua>
2021-02-12 22:15:05 +0200

    Added Sabre

---
+++ src/main/java/net/mcreator/cossacksadventure/item/SabreItem.java
@@ -0,0 +1,63 @@
+
+package net.mcreator.cossacksadventure.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.cossacksadventure.CossacksAdventureModElements;
+
+import java.util.List;
+
+@CossacksAdventureModElements.ModElement.Tag
+public class SabreItem extends CossacksAdventureModElements.ModElement {
+	@ObjectHolder("cossacks_adventure:sabre")
+	public static final Item block = null;
+	public SabreItem(CossacksAdventureModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1000;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 15;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -2.2f, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+				super.addInformation(itemstack, world, list, flag);
+				list.add(new StringTextComponent("Classic Cossacks` weapon"));
+			}
+		}.setRegistryName("sabre"));
+	}
+}
